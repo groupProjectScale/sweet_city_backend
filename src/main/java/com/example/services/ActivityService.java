58ee56af128d2df9da013295aebe1ad1906abd83
HongxiChen1014 <hongxi_chen2020@163.com>
2023-03-03 19:14:30 -0800

    implemented requirement api

--- src/main/java/com/example/services/ActivityService.java
+++ src/main/java/com/example/services/ActivityService.java
@@ -3,12 +3,18 @@ package com.example.services;
 import static org.springframework.transaction.annotation.Isolation.SERIALIZABLE;
 
 import com.example.dto.ActivityDto;
+import com.example.dto.RequirementDto;
+import com.example.dto.TagDto;
 import com.example.dto.UserLoginDto;
 import com.example.model.Activity;
 import com.example.model.Address;
+import com.example.model.Requirement;
+import com.example.model.Tag;
 import com.example.model.User;
 import com.example.repository.ActivityRepository;
 import com.example.repository.AddressRepository;
+import com.example.repository.RequirementRepository;
+import com.example.repository.TagRepository;
 import com.example.repository.UserRepository;
 import java.util.List;
 import java.util.Optional;
@@ -24,14 +30,20 @@ public class ActivityService {
     private final ActivityRepository activityRepository;
     private final UserRepository userRepository;
     private final AddressRepository addressRepository;
+    private final TagRepository tagRepository;
+    private final RequirementRepository requirementRepository;
 
     public ActivityService(
             ActivityRepository activityRepository,
             UserRepository userRepository,
-            AddressRepository addressRepository) {
+            AddressRepository addressRepository,
+            TagRepository tagRepository,
+            RequirementRepository requirementRepository) {
         this.activityRepository = activityRepository;
         this.userRepository = userRepository;
         this.addressRepository = addressRepository;
+        this.tagRepository = tagRepository;
+        this.requirementRepository = requirementRepository;
     }
 
     public Optional<Activity> getActivityById(UUID activityId) {
@@ -132,5 +144,70 @@ public class ActivityService {
         activityRepository.save(activity.get());
         activityRepository.removeOneParticipant(activityId);
         return true;
+    }
+
+    @Transactional(
+            rollbackFor = {RuntimeException.class, Exception.class},
+            isolation = SERIALIZABLE)
+    public boolean addTagForActivity(UUID activityId, TagDto tagDto) {
+        Optional<Activity> activity = activityRepository.findById(activityId);
+        if (activity.isEmpty()) {
+            return false;
+        }
+        Tag tag = tagRepository.findByTagDescription(tagDto.getTagDescription());
+        // do not exist this tag before, store tagDescription
+        if (tag == null) {
+            tag = addTag(tagDto);
+        }
+        // tag already exists in this activity
+        if (activity.get().getTags().contains(tag)) {
+            return false;
+        }
+        activity.get().addTag(tag);
+        activityRepository.save(activity.get());
+        tagRepository.addOneCreation(tag.getTagId());
+        return true;
+    }
+
+    @Transactional(isolation = SERIALIZABLE)
+    Tag addTag(TagDto tagDto) {
+        Tag tag = tagRepository.findByTagDescription(tagDto.getTagDescription());
+        if (tag != null) {
+            return tag;
+        }
+        Tag t = new Tag();
+        BeanUtils.copyProperties(tagDto, t);
+        return tagRepository.save(t);
+    }
+
+    @Transactional
+    public boolean addRequirementForActivity(UUID activityId, RequirementDto requirementDto) {
+        Optional<Activity> activity = activityRepository.findById(activityId);
+        if (activity.isEmpty()) {
+            return false;
+        }
+        Requirement r = requirementRepository.findByDescription(requirementDto.getDescription());
+        // do not exist this requirement before, store requirement
+        if (r == null) {
+            r = addRequirement(requirementDto);
+        }
+        // requirement already exists in this activity
+        if (activity.get().getRequirements().contains(r)) {
+            return false;
+        }
+        activity.get().addRequirements(r);
+        activityRepository.save(activity.get());
+        return true;
+    }
+
+    @Transactional(isolation = SERIALIZABLE)
+    Requirement addRequirement(RequirementDto requirementDto) {
+        Requirement r = new Requirement();
+        Requirement req = requirementRepository.findByDescription(requirementDto.getDescription());
+        if (req != null) {
+            return req;
+        }
+        BeanUtils.copyProperties(requirementDto, r);
+        return requirementRepository.save(r);
     }
 }
