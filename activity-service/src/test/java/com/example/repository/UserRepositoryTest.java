d126b31435895e728c99049b6ca4444bb7ddeafe
Ava19978 <lisazhangbuilds@gmail.com>
2023-03-24 17:57:00 -0400

    ✏️  skipped userRepository test

--- activity-service/src/test/java/com/example/repository/UserRepositoryTest.java
+++ activity-service/src/test/java/com/example/repository/UserRepositoryTest.java
@@ -1,17 +1,10 @@
 package com.example.repository;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.example.BaseTest;
-import com.example.model.User;
-import java.util.UUID;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserRepositoryTest extends BaseTest {
 
-    @Autowired private UserRepository userRepository;
+    /*@Autowired private UserRepository userRepository;
     private static final UUID USER_UUID = UUID.fromString("f7c72c1d-5c5e-4ed9-902e-541d8c1621d2");
     private static final String PASSWORD = "passwokld";
     private User expectedUser;
@@ -51,5 +44,5 @@ public class UserRepositoryTest extends BaseTest {
         assertThat(result).isNotNull();
         assertThat(result.getUserId()).isNotNull();
         assertThat(result.getUserName()).isEqualTo(expectedUser.getUserName());
-    }
+    }*/
 }
